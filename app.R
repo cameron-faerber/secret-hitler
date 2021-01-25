a665c7712a6530c44d72a3b7595087cda35392c6
cameron-faerber <cameron.faerber@gmail.com>
2021-01-25 01:22:59 -0500

    great updates

--- app.R
+++ app.R
@@ -13,7 +13,6 @@ ui <- fluidPage(
 
     mainPanel(
         textOutput("count"),
-        br(),
         setSliderColor(c("dodgerblue","firebrick"), c(1,2)),
         sliderTextInput(
             inputId = "nLiberal",
@@ -34,42 +33,65 @@ ui <- fluidPage(
             color = "success"
         ),
         hr(),
+        fluidRow(column(2,imageOutput("card1")),
+                 column(2,imageOutput("card2")),
+                 column(2,imageOutput("card3"))
+                 ),
         uiOutput("cards"),
-        br(),
-        br(),
-        br()
         ),
     
     ## keep alive here ##
     tags$head(
         HTML(
-            "
-          <script>
-          var socket_timeout_interval
-          var n = 0
-          $(document).on('shiny:connected', function(event) {
-          socket_timeout_interval = setInterval(function(){
-          Shiny.onInputChange('count', n++)
-          }, 15000)
-          });
-          $(document).on('shiny:disconnected', function(event) {
-          clearInterval(socket_timeout_interval)
-          });
-          </script>
-          "
+        "
+        <script>
+            var socket_timeout_interval
+            var n = 0
+            $(document).on('shiny:connected', function(event) {
+            socket_timeout_interval = setInterval(function(){
+                Shiny.onInputChange('count', n++)
+                    }, 15000)
+                });
+            $(document).on('shiny:disconnected', function(event) {
+                clearInterval(socket_timeout_interval)
+            });
+        </script>
+        "
         )
     ),
     span(textOutput("keepAlive"), style="color:white"),
-
     )
 
 # Define server logic
 server <- function(input, output){
-    i <<- 1
-    
+    drawValues = reactiveValues()
     draw = eventReactive(input$drawCards, {
-        cards = rbinom(n=3, size=1, prob=11/17)
-        ifelse(cards==1, "facist.png", "liberal.png")
+        random_draw = rbinom(n=3, size=1, prob=11/17)
+        cards = ifelse(random_draw==1, "facist.png", "liberal.png")
+        drawValues$value = cards
+        drawValues[['hiddenState']] = c(0,0,0)
+        
+        startAnim(session = getDefaultReactiveDomain(), "card1","bounce")
+        startAnim(session = getDefaultReactiveDomain(), "card2","bounce")
+        startAnim(session = getDefaultReactiveDomain(), "card3","bounce")
+        
+        
+        return(cards)
+    })
+    
+    onclick("card1", {
+        drawValues$value[1] = "hidden.png"
+        drawValues[['hiddenState']][1] = 1 - drawValues[['hiddenState']][1] 
+    })
+    
+    onclick("card2", {
+        drawValues$value[2] = "hidden.png"
+        drawValues[['hiddenState']][2] = 1 - drawValues[['hiddenState']][2] 
+    })
+    
+    onclick("card3", {
+        drawValues$value[3] = "hidden.png"
+        drawValues[['hiddenState']][3] = 1 - drawValues[['hiddenState']][3] 
     })
     
     #count number of draws
@@ -81,29 +103,48 @@ server <- function(input, output){
         paste("Number of draws: ", counter$countervalue)
     })
     
-    output$cards <- renderUI({
+    output$card1 = renderImage({
         images = draw()
+        image = images[1]
+        if(drawValues[['hiddenState']][1]==1){
+            image = 'hidden.png'
+        }
         
-        startAnim(
-            session = getDefaultReactiveDomain(), 
-            "cards",
-            "bounce"
-        )
-        fluidRow(id="los-cards",
-                 column(2,img(id="card1", src=images[1], width=100, height=135)),
-                 column(2,img(id="card2", src=images[2], width=100, height=135)),
-                 column(2,img(id="card3", src=images[3], width=100, height=135))
-        )
-    })
+        list(src = paste0("www/", image), 
+             contentType = 'image/png',
+             width = 100,
+             height = 135)
+    }, deleteFile = FALSE)
+    
+    output$card2 = renderImage({
+        images = draw()
+        image = images[2]
+        if(drawValues[['hiddenState']][2]==1){
+            image = 'hidden.png'
+        }
+        list(src = paste0("www/", image), 
+             contentType = 'image/png',
+             width = 100,
+             height = 135)
+    }, deleteFile = FALSE)
+    
+    output$card3 = renderImage({
+        images = draw()
+        image = images[3]
+        if(drawValues[['hiddenState']][3]==1){
+            image = 'hidden.png'
+        }
+        list(src = paste0("www/", image), 
+             contentType = 'image/png',
+             width = 100,
+             height = 135)
+    }, deleteFile = FALSE)
+
     
     ## keep alive ##
     output$keepAlive <- renderText({
         req(input$count)
         paste("keep alive ", input$count)
-    })
-
-    observeEvent(input$drawCards, {
-        startAnim(session = getDefaultReactiveDomain(), "los-cards", "bounceInLeft")
     })
 
 }
